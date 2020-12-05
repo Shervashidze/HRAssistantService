1da393adf9d2faf986e5d88c7eb047c919048605
George Shervashidze <gshervashidze1999@yandex.ru>
2020-12-05 14:29:43 +0300

    renaming and initial commit for learning service

---
+++ Coursework/LearningService/Models/LearningEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LearningService.Models
+{
+    public class LearningEvent
+    {
+        [Key]
+        public long Id { get; set; }
+    }
+}
