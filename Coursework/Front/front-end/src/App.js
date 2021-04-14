6fcfb2ebe845252e0e9344f05eb010822d3b7df2
George <gshervashidze1999@yandex.ru>
2021-04-14 11:38:40 +0300

    adding login page

--- Coursework/Front/front-end/src/App.js
+++ Coursework/Front/front-end/src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AuthService from "./services/auth.service";
 
 import WorkersPage from './pages/WorkersPage';
 import LearningPage from "./pages/LearningPage";
 import DashboardsPage from "./pages/DashboardsPage";
 import BlankPage from "./pages/blankPage";
 import EditWorker from './pages/EditWorker';
+import Login from './components/login.component.js';
 
-function App() {
-  return (
-    <React.Fragment>
-      <Router>
-        <Route exact path="/workers" component={WorkersPage} />
-        <Route exact path="/learning" component={LearningPage} />
-        <Route exact path="/learning/:id" component={BlankPage}/>
-        <Route exact path="/dashboards" component={DashboardsPage} />
-      </Router>
-    </React.Fragment>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <Route exact path="/workers" component={WorkersPage} />
+          <Route exact path="/learning" component={LearningPage} />
+          <Route exact path="/learning/:id" component={BlankPage}/>
+          <Route exact path="/dashboards" component={DashboardsPage} />
+          <Route exact path="/login" component={Login} />
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
+
+
 
 export default App;
